9795786c689126866870bc4ff5b19b36c6043f89
alenorgue <alenoriegag@gmail.com>
2025-08-18 21:39:44 +0200

    Add IdCard component with props for user details and display logic

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <script setup>
-
+import IdCard from './components/IdCard.vue'
 </script>
 
 <template>
   <h1>Creación de componentes con Vue</h1>
   <p>Crea a conitnuación todos los componentes del ejercicio.</p>
+      <h1>1- ID Cards</h1>
+<div>
+  <IdCard
+    picture="https://randomuser.me/api/portraits/men/44.jpg"
+    first-name="John"
+    last-name="Doe"
+    gender="male"
+    :height="178"
+  :birth="new Date('1992-07-14')"
+  />
+ <IdCard
+  last-name='Delores '
+  first-name='Obrien'
+  gender='female'
+  :height="172"
+  :birth='new Date("1988-05-11")'
+  picture="https://randomuser.me/api/portraits/women/44.jpg"
+/>
+</div>
 </template>
 
 <style scoped>
-
+h2 {margin: 1rem 0;}
 </style>
---
+++ src/components/IdCard.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { computed } from 'vue';
+
+
+const props = defineProps({
+    lastName: { type: String, required: true },
+    firstName: { type: String, required: true },
+    gender: { type: String, required: true }, 
+    height: { type: Number, required: true },
+  birth: { type: Date, required: true },
+    picture: { type: String, required: true }
+})
+
+
+const formattedBirth = computed(() => props.birth.toDateString());
+
+
+</script>
+
+<template>
+      <div class="id-card-container">
+        <img :src="picture" alt="Profile picture" class="id-card-img" />
+        <div class="id-card-details">
+          <p> <strong> First name: </strong>{{ firstName }} </p>
+          <p> <strong> Last name: </strong>{{ lastName }} </p>
+          <p> <strong> Gender: </strong> {{ gender }}</p>
+          <p> <strong> Height: </strong> {{ height }}</p>
+          <p> <strong> Birth: </strong> {{ formattedBirth }}</p>
+        </div>
+      </div>
+</template>
+
+<style scoped>
+ .id-card-container {
+   display: flex;
+   align-items: center;
+    box-shadow: 0 2px 8px rgba(0,0,0,0.08);
+   padding: 1rem;
+   margin: 1rem;
+  border: solid black 1px;
+ }
+ .id-card-img {
+   width: 150px;
+   height: 150px;
+   object-fit: cover;
+   margin-right: 1.5rem;
+ }
+ .id-card-details {
+   flex: 1;
+ }
+
+</style>
+
+ 
